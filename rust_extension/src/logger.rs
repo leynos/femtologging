1beed31a5f33cf6a0e4a58e9dc3e977573844966
Leynos <leynos@troubledskies.net>
2025-06-27 16:54:19 +0100

    Apply formatter in FemtoLogger

--- rust_extension/src/logger.rs
+++ rust_extension/src/logger.rs
@@ -5,7 +5,8 @@ use pyo3::prelude::*;
 use crossbeam_channel::{bounded, Receiver, Sender};
 use std::thread::{self, JoinHandle};
 
-use crate::log_record::FemtoLogRecord;
+use crate::{formatter::{DefaultFormatter, FemtoFormatter}, log_record::FemtoLogRecord};
+use std::sync::Arc;
 
 const DEFAULT_CHANNEL_CAPACITY: usize = 1024;
 
@@ -14,6 +15,7 @@ const DEFAULT_CHANNEL_CAPACITY: usize = 1024;
 pub struct FemtoLogger {
     /// Identifier used to distinguish log messages from different loggers.
     name: String,
+    formatter: Arc<dyn FemtoFormatter>,
     tx: Option<Sender<FemtoLogRecord>>,
     handle: Option<JoinHandle<()>>,
 }
@@ -28,13 +30,20 @@ impl FemtoLogger {
         // producers outpace the consumer thread.
         let (tx, rx): (Sender<FemtoLogRecord>, Receiver<FemtoLogRecord>) =
             bounded(DEFAULT_CHANNEL_CAPACITY);
+
+        // Default to a simple formatter mirroring Python's "name: level - message" format.
+        let formatter: Arc<dyn FemtoFormatter> = Arc::new(DefaultFormatter);
+        let thread_formatter = Arc::clone(&formatter);
+
         let handle = thread::spawn(move || {
             for record in rx {
-                println!("{}", record);
+                println!("{}", thread_formatter.format(&record));
             }
         });
+
         Self {
             name,
+            formatter,
             tx: Some(tx),
             handle: Some(handle),
         }
@@ -47,7 +56,7 @@ impl FemtoLogger {
     #[pyo3(text_signature = "(self, level, message)")]
     pub fn log(&self, level: &str, message: &str) -> String {
         let record = FemtoLogRecord::new(&self.name, level, message);
-        let msg = format!("{}: {}", self.name, record);
+        let msg = self.formatter.format(&record);
         if let Some(tx) = &self.tx {
             if tx.send(record).is_err() {
                 eprintln!("Warning: failed to send log record to background thread");
