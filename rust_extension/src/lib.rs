f6cf72101dd3f16e6133b9edc4a72f6daabe0a72
Leynos <leynos@troubledskies.net>
2025-07-21 20:31:13 +0100

    Drop deprecated module init

--- rust_extension/src/lib.rs
+++ rust_extension/src/lib.rs
@@ -41,13 +41,8 @@ fn reset_manager_py() {
     reset_manager();
 }
 
-#[expect(
-    deprecated,
-    reason = "module init signature uses Bound for PyO3 0.25; will be cleaned up in issue #92"
-)]
-#[allow(unfulfilled_lint_expectations)]
 #[pymodule]
-fn _femtologging_rs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _femtologging_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<FemtoLogger>()?;
     m.add_class::<FemtoHandler>()?;
     m.add_class::<FemtoStreamHandler>()?;
