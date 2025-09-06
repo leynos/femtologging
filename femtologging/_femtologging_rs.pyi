63da4ca04d1c48064829ec032e6f2a4f0b93c893
Leynos <leynos@troubledskies.net>
2025-09-06 20:14:17 +0100

    Stub Rust extension for type check

---
+++ femtologging/_femtologging_rs.pyi
@@ -0,0 +1,20 @@
+from typing import Any as _Any
+
+FemtoLogger: _Any
+FemtoHandler: _Any
+FemtoStreamHandler: _Any
+FemtoFileHandler: _Any
+StreamHandlerBuilder: _Any
+FileHandlerBuilder: _Any
+ConfigBuilder: _Any
+LoggerConfigBuilder: _Any
+FormatterBuilder: _Any
+LevelFilterBuilder: _Any
+NameFilterBuilder: _Any
+FilterBuildError: type[Exception]
+HandlerConfigError: type[Exception]
+HandlerIOError: type[Exception]
+
+hello: _Any
+get_logger: _Any
+reset_manager_py: _Any
